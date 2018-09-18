b5716b1ec0101f0c37b917bcfa049b15cd262f9c
Eric Lemes <ericlemes@gmail.com>
2018-09-18 09:46:48 +0100

    Adds missing tests.

---
+++ src/p4codechurn.unittests/GivenAnEnvironmentImpl.cs
@@ -0,0 +1,20 @@
+﻿using p4codechurn.core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace p4codechurn.unittests
+{
+    public class GivenAnEnvironmentImpl
+    {
+        [Fact]
+        public void WhenGettingCurrentDateAndTimeShouldNotThrow()
+        {
+            var environment = new EnvironmentImpl();
+            environment.GetCurrentDateTime();
+        }
+    }
+}
