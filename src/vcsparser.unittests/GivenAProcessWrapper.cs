9874de1586a75daecf321218e39a29bf4fa46dab
Eric Lemes <ericlemes@gmail.com>
2019-01-29 17:14:11 +0000

    Removing more tests

--- src/vcsparser.unittests/GivenAProcessWrapper.cs
+++ src/vcsparser.unittests/GivenAProcessWrapper.cs
@@ -11,22 +11,6 @@ namespace vcsparser.unittests
 {
     public class GivenAProcessWrapper
     {
-        [Fact]
-        public void WhenInvokingShouldCallProcessAndRedirectStdOut()
-        {
-            var processWrapper = new ProcessWrapper();
-            var rd = new StreamReader(processWrapper.Invoke("cmd", "/c dir"));
-            string output = rd.ReadToEnd();
-            Assert.NotEmpty(output);            
-        }
 
-        [Fact]
-        public void WhenInvokingWithWorkingDirShouldCallProcessAndRedirectStdOut()
-        {
-            var processWrapper = new ProcessWrapper();
-            var rd = new StreamReader(processWrapper.Invoke("cmd", "/c dir", "C:\\"));
-            string output = rd.ReadToEnd();
-            Assert.NotEmpty(output);
-        }
     }
 }
